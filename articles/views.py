07e9bb3d3b325f79a93de8b10cb1a1699507426f
hyejin beck <beckhye@gmail.com>
2023-08-29 15:33:18 +0900

    로그인상황시에만 댓글확인

--- articles/views.py
+++ articles/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render , redirect
 from .models import Article # 추가 
-from .forms import ArticleForm 
+from .forms import ArticleForm  , CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     
-    articles = Article.objects.all() # 전체게시글 조회 
+    articles = Article.objects.all() 
+    form = CommentForm()
 
     context = {
         'articles' : articles, 
+        'form': form,
     }
     return render(request,'index.html', context)
 
@@ -35,3 +37,19 @@ def create(request):
     }
 
     return render(request, 'form.html', context)
+
+@login_required
+def comment_create(request,article_id): 
+    article = Article.objects.get(id=article_id)
+
+    # 댓글작성 폼 
+    form = CommentForm(request.POST)
+    
+    if form.is_valid(): # 댓글을 잘 작성했다면 
+        comment = form.save(commit=False)
+        # 아직 user값 없어서 저장하지마
+        comment.user = request.user
+        comment.article = article 
+        comment.save()
+
+        return redirect('articles:index')
